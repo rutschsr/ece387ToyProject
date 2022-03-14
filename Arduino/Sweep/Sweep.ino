866a1412080806c998cafb0bc8118ecc778b2052
Sam Rutschilling <56640827+rutschsr@users.noreply.github.com>
2022-03-13 23:20:42 -0400

    Test programs

--- Arduino/Sweep/Sweep.ino
+++ Arduino/Sweep/Sweep.ino
@@ -11,24 +11,33 @@
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
+Servo Servo2;
 
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9);
+  Servo2.attach(10);// attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 50; pos <= 138; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo.write(pos);
+    Servo2.write(180-pos); // tell servo to go to position in variable 'pos'
     delay(5);                       // waits 15ms for the servo to reach the position
   }
-  
-  for (pos = 90; pos >= 10; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 15ms for the servo to reach the position
-  }
-  myservo.write(10);              // tell servo to go to position in variable 'pos'
+    myservo.write(138);
+    Servo2.write(180-138);// tell servo to go to position in variable 'pos'
     delay(1000); 
+  
+  for (pos = 138; pos >= 50; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);
+    Servo2.write(180-pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+  myservo.write(50);
+  Servo2.write(180-50);
+  // tell servo to go to position in variable 'pos'
+    delay(5000); 
 }
